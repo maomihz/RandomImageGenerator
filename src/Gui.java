bf3122639d8927b4513a51a2906e2332104fb68c
Seancheey <adls371@163.com>
2015-04-16 22:13:30 -0700

    format the Gui class

--- src/Gui.java
+++ src/Gui.java
@@ -8,82 +8,93 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-public class Gui extends JFrame{
-	JButton btnRepaint,btnSave;
+
+public class Gui extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JButton btnRepaint, btnSave;
 	private JTextField widthField;
 	private JTextField heightField;
 	private JTextField pixelWidthField;
 	private JTextField pixelHeightField;
 	private JTextField fileNameField;
 	private JTextField numberField;
-	
-	private JPanel imagePanel;
-	
-	//the picture
-	private BufferedImage image = new BufferedImage(345,55,BufferedImage.TYPE_INT_ARGB);
-	
-	//image is gray scale
+
+	private JPanel imagePanel, operationPanel, presetPanel;
+
+	// the picture
+	private BufferedImage image = new BufferedImage(345, 55,
+			BufferedImage.TYPE_INT_ARGB);
+
+	// image is gray scale
 	boolean isGrayScale;
 	boolean saveMultiple;
 	int fileNum;
-	
-	//image width
-	int width,height;
 
+	// image width
+	int width, height;
 
-	public Gui() throws IOException{
-		setSize(800,600);
+	public Gui() throws IOException {
+		setSize(800, 600);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setTitle("Random Image Generator by Maomi");
 		getContentPane().setLayout(null);
-		setIconImage(ImageIO.read(new File("res" + File.separator + "icon.png")));
+		setIconImage(ImageIO
+				.read(new File("res" + File.separator + "icon.png")));
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
 		setResizable(false);
 
+		operationPanel = new JPanel() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
 
-		JPanel operationPanel = new JPanel() {
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				g.setFont(new Font("Serif", Font.PLAIN,48));
+				g.setFont(new Font("Serif", Font.PLAIN, 48));
 				g.drawString("Settings", 80, 50);
 			}
 		};
-		operationPanel.setSize(300,450);
+		operationPanel.setSize(300, 450);
 		operationPanel.setBackground(Color.LIGHT_GRAY);
 		operationPanel.setLocation(0, 0);
 		getContentPane().add(operationPanel);
 		operationPanel.setLayout(null);
-		
+
 		final LoadingWheel animation = new LoadingWheel(50);
 		animation.setLocation(10, 10);
 		operationPanel.add(animation);
 		animation.setVisible(false);
-		
-		
+
 		final JPanel previewPanel = new JPanel();
-		previewPanel.setSize(500,600);
+		previewPanel.setSize(500, 600);
 		previewPanel.setBackground(SystemColor.window);
 		previewPanel.setLocation(300, 0);
 		getContentPane().add(previewPanel);
 		previewPanel.setLayout(null);
-		
+
 		imagePanel = new JPanel() {
 			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
+
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
 				g.drawImage(image, 0, 0, null);
 
 			}
+
 			public Dimension getPreferredSize() {
 				return new Dimension(image.getWidth(), image.getHeight());
 			}
 		};
 		imagePanel.setSize(imagePanel.getPreferredSize());
-		imagePanel.setLocation(0,0);
+		imagePanel.setLocation(0, 0);
 		previewPanel.add(imagePanel);
 
 		JLabel lblHeight = new JLabel("Height");
@@ -129,13 +140,13 @@ public class Gui extends JFrame{
 		pixelHeightField.setBounds(128, 184, 134, 28);
 		operationPanel.add(pixelHeightField);
 		pixelHeightField.setColumns(10);
-		
+
 		final JLabel lblNumber = new JLabel("Number");
 		lblNumber.setVisible(false);
 		lblNumber.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNumber.setBounds(10, 316, 118, 16);
 		operationPanel.add(lblNumber);
-		
+
 		numberField = new JTextField();
 		numberField.setText("1");
 		numberField.setVisible(false);
@@ -146,7 +157,7 @@ public class Gui extends JFrame{
 		final JCheckBox chckbxGrayscale = new JCheckBox("GrayScale");
 		chckbxGrayscale.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				if (((JCheckBox)e.getSource()).isSelected()) {
+				if (((JCheckBox) e.getSource()).isSelected()) {
 					isGrayScale = true;
 				} else {
 					isGrayScale = false;
@@ -161,7 +172,7 @@ public class Gui extends JFrame{
 		final JCheckBox chckbxSaveMultiple = new JCheckBox("Save Multiple");
 		chckbxSaveMultiple.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				if (((JCheckBox)e.getSource()).isSelected()) {
+				if (((JCheckBox) e.getSource()).isSelected()) {
 					numberField.setVisible(true);
 					lblNumber.setVisible(true);
 					saveMultiple = true;
@@ -190,27 +201,35 @@ public class Gui extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				new Thread() {
 					public void run() {
-						
+
 						int fileNameCount = 0;
 						int fileNumCount = 0;
 						int fileNum = Integer.parseInt(numberField.getText());
 						File destFolder = new File("RandomImages");
-						if (!destFolder.exists()) destFolder.mkdir();
-						
+						if (!destFolder.exists())
+							destFolder.mkdir();
+
 						while (fileNumCount < fileNum) {
 							File destination;
 							do {
 								animation.setVisible(true);
-								destination = new File("RandomImages" + File.separator + fileNameField.getText() + "_" + image.getWidth() + "x" + image.getHeight() + "_" + fileNameCount + ".png");
+								destination = new File("RandomImages"
+										+ File.separator
+										+ fileNameField.getText() + "_"
+										+ image.getWidth() + "x"
+										+ image.getHeight() + "_"
+										+ fileNameCount + ".png");
 								fileNameCount++;
-								System.out.println(destination + " Name exist!!!");
+								System.out.println(destination
+										+ " Name exist!!!");
 							} while (destination.exists());
 							try {
 								animation.setVisible(true);
 								ImageIO.write(image, "png", destination);
 								fileNumCount++;
 								System.out.println("I wrote an image");
-							} catch (IOException e1) {}
+							} catch (IOException e1) {
+							}
 							update();
 						}
 						animation.setVisible(false);
@@ -221,7 +240,6 @@ public class Gui extends JFrame{
 		btnSave.setBounds(119, 350, 75, 29);
 		operationPanel.add(btnSave);
 
-		
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -245,15 +263,16 @@ public class Gui extends JFrame{
 		JLabel lblpng = new JLabel(".png");
 		lblpng.setBounds(199, 282, 39, 16);
 		operationPanel.add(lblpng);
-		
-		final JPanel presetPanel = new JPanel();
-		presetPanel.setBorder(new TitledBorder(null, "Presets", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		presetPanel.setSize(300,150);
-		presetPanel.setBackground(new Color(225,225,225));
-		presetPanel.setLocation(0,450);
+
+		presetPanel = new JPanel();
+		presetPanel.setBorder(new TitledBorder(null, "Presets",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		presetPanel.setSize(300, 150);
+		presetPanel.setBackground(new Color(225, 225, 225));
+		presetPanel.setLocation(0, 450);
 		getContentPane().add(presetPanel);
 		presetPanel.setVisible(false);
-		
+
 		JButton presetKindle = new JButton("Kindle");
 		presetKindle.setBounds(6, 24, 83, 29);
 		presetKindle.addActionListener(new ActionListener() {
@@ -266,7 +285,7 @@ public class Gui extends JFrame{
 		});
 		presetPanel.setLayout(null);
 		presetPanel.add(presetKindle);
-		
+
 		JButton presetIphone = new JButton("iPhone");
 		presetIphone.setBounds(82, 24, 86, 29);
 		presetIphone.addActionListener(new ActionListener() {
@@ -278,7 +297,7 @@ public class Gui extends JFrame{
 			}
 		});
 		presetPanel.add(presetIphone);
-		
+
 		JButton presetIphoneRetina = new JButton("iPhone(Retina)");
 		presetIphoneRetina.setBounds(161, 24, 133, 29);
 		presetIphoneRetina.addActionListener(new ActionListener() {
@@ -290,7 +309,7 @@ public class Gui extends JFrame{
 			}
 		});
 		presetPanel.add(presetIphoneRetina);
-		
+
 		JButton presetIpad = new JButton("iPad");
 		presetIpad.setBounds(92, 49, 75, 29);
 		presetIpad.addActionListener(new ActionListener() {
@@ -302,7 +321,7 @@ public class Gui extends JFrame{
 			}
 		});
 		presetPanel.add(presetIpad);
-		
+
 		JButton presetIpadRetina = new JButton("iPad(Retina)");
 		presetIpadRetina.setBounds(171, 49, 117, 29);
 		presetIpadRetina.addActionListener(new ActionListener() {
@@ -314,7 +333,7 @@ public class Gui extends JFrame{
 			}
 		});
 		presetPanel.add(presetIpadRetina);
-		
+
 		JButton presetComputer1024_768 = new JButton("Computer(1024x768)");
 		presetComputer1024_768.setBounds(16, 77, 178, 29);
 		presetComputer1024_768.addActionListener(new ActionListener() {
@@ -326,9 +345,7 @@ public class Gui extends JFrame{
 			}
 		});
 		presetPanel.add(presetComputer1024_768);
-		
-		
-		
+
 		JButton btnShowPresets = new JButton("Show Presets");
 		btnShowPresets.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -349,44 +366,49 @@ public class Gui extends JFrame{
 		update();
 	}
 
-	
-	private void drawImage(BufferedImage image, boolean isGrayScale, int pixelWidth, int pixelHeight) {
-		if (pixelWidth <= 0 || pixelHeight <= 0 || image == null || image.getHeight(null) <= 0 || image.getWidth(null) <= 0) {
+	private void drawImage(BufferedImage image, boolean isGrayScale,
+			int pixelWidth, int pixelHeight) {
+		if (pixelWidth <= 0 || pixelHeight <= 0 || image == null
+				|| image.getHeight(null) <= 0 || image.getWidth(null) <= 0) {
 			throw new IllegalArgumentException();
 		}
-		int xCellNum = (int)Math.ceil(image.getWidth(null) / (double)pixelWidth);
-		int yCellNum = (int)Math.ceil(image.getHeight(null) / (double)pixelHeight);
+		int xCellNum = (int) Math.ceil(image.getWidth(null)
+				/ (double) pixelWidth);
+		int yCellNum = (int) Math.ceil(image.getHeight(null)
+				/ (double) pixelHeight);
 		Graphics g = image.getGraphics();
-		for (int j=0;j<yCellNum;j++) {
-			for (int i=0;i<xCellNum;i++) {
+		for (int j = 0; j < yCellNum; j++) {
+			for (int i = 0; i < xCellNum; i++) {
 				g.setColor(randColor(isGrayScale));
-				g.fillRect(i*pixelWidth, j*pixelHeight, pixelWidth, pixelHeight);
+				g.fillRect(i * pixelWidth, j * pixelHeight, pixelWidth,
+						pixelHeight);
 			}
 		}
 	}
-	
+
 	public static Color randColor(boolean isGrayScale) {
 		if (isGrayScale) {
-			int c = (int)(Math.random() * 256);
-			return new Color(c,c,c);
+			int c = (int) (Math.random() * 256);
+			return new Color(c, c, c);
 		} else {
-			return new Color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256));
+			return new Color((int) (Math.random() * 256),
+					(int) (Math.random() * 256), (int) (Math.random() * 256));
 		}
 	}
-	
+
 	private void update() {
 		int width = Integer.parseInt(widthField.getText());
 		int height = Integer.parseInt(heightField.getText());
 		int pixelWidth = Integer.parseInt(pixelWidthField.getText());
 		int pixelHeight = Integer.parseInt(pixelHeightField.getText());
 		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		drawImage(image,isGrayScale,pixelWidth,pixelHeight);
+		drawImage(image, isGrayScale, pixelWidth, pixelHeight);
 		repaint();
 		imagePanel.setSize(imagePanel.getPreferredSize());
 		System.out.println("Image Regenerated");
 	}
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		Gui window = new Gui();
 		window.setVisible(true);
 	}
