4b690724feccbee9dd87631744f3cdc2571ffaf5
MaomiHz <maomihz@gmail.com>
2015-04-19 07:11:14 +0800

    NO window auto-resize

--- src/com/maomihz/gui/Gui.java
+++ src/com/maomihz/gui/Gui.java
@@ -51,8 +51,8 @@ public class Gui extends JFrame implements GridBag {
 
 	public void update() {
 		previewPanel.getImagePanel().update();
-		setSize(getPreferredSize());
-		setLocationRelativeTo(null);
+		//setSize(getPreferredSize());
+		//setLocationRelativeTo(null);
 	}
 
 	public static Color randColor() {
